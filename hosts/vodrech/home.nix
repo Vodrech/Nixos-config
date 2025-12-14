7673c1e1dfd5a5f5b4c83250227344f324966f31
Vodrech <viiddee@hotmail.com>
2025-12-14 15:52:56 +0100

    boot not fixing trying stuff

--- hosts/vodrech/home.nix
+++ hosts/vodrech/home.nix
@@ -4,12 +4,4 @@
 	home.username = "vodrech";
 	home.homeDirectory = "/home/vodrech";
 	home.stateVersion = "25.11";
-	programs.bash = {
-		enable=true;
-		profileExtra = ''
-		if [ -z "$WAYLAND_DISPlAY" ] && [ "$XDG_VTNR" = 1 ]; then
-			ecec hyprland
-		fi
-		''
-	}
 }
